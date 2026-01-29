59e55ab407a9c255de1b58ebfc61d4e4ac12f57a
Dylan <dylanmanbarker@gmail.com>
2026-01-29 14:30:11 -0800

    ADDED: Basic Scripts

---
+++ Assets/Scripts/Ingredients.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Ingredients : MonoBehaviour
+{
+    // Type of food ingredient
+    // Prep methods could include: Chopped, Sliced, Diced, Whole, etc.
+    // Cook Methods could include: Boiled, Fried, Baked, Grilled, etc.
+}
